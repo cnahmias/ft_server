4121b29ac45fbee0f3b87362dc02ac334799820c
sg9031 <sg9031@gmail.com>
2021-03-30 17:47:19 +0400

    .

--- src/init.sh
+++ src/init.sh
@@ -4,11 +4,11 @@ mysql -u user root init.sql
 rm init.sql
 
 # SSH
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/chez-sgrondin.key -out /etc/ssl/certs/chez-sgrondin.crt -subj "/C=FR/ST=Paris/L=Île-de-France/O=42/OU=sgrondin/CN=chez-sgrondin"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-ssl.key -out /etc/ssl/certs/nginx-ssl.crt -subj "/C=FR/ST=Paris/L=Île-de-France/O=42/OU=sgrondin/CN=chez-sgrondin"
 
 # nginx
-mv nginx.conf /etc/nginx/sites-available/chez-sgrondin.conf
-ln -s /etc/nginx/sites-available/chez-sgrondin.conf /etc/nginx/sites-enabled/
+mv nginx.conf /etc/nginx/sites-available/
+ln -s /etc/nginx/sites-available/nginx.conf /etc/nginx/sites-enabled/
 rm -rf /etc/nginx/sites-enabled/default
 
 # Wordpress
@@ -23,11 +23,18 @@ mv wp-config-sample.php wp-config.php
 sed -i 's/database_name_here/chez-sgrondin/' wp-config.php
 sed -i 's/username_here/wpuser/' wp-config.php
 sed -i 's/password_here/verysecurepassword/' wp-config.php
-rm index.php
+cd /var/www/html
 echo '<?php phpinfo();?>' > index.php
 cd /
 chown www-data:www-data /var/www/html/chez-sgrondin -R
 chmod -R -wx,u+rwX,g+rX,o+rX /var/www/html/chez-sgrondin
+
+# PHP MyAdmin
+cd /var/www/html/
+wget https://files.phpmyadmin.net/phpMyAdmin/5.1.0/phpMyAdmin-5.1.0-all-languages.tar.gz
+tar xfz phpMyAdmin-5.1.0-all-languages.tar.gz
+mv phpMyAdmin-5.1.0-all-languages php-myadmin
+rm phpMyAdmin-5.1.0-all-languages.tar.gz
 
 service nginx start
 service php7.3-fpm start
